ea90adca3cdd325972cbb04136d1f4386ce5cc0e
残月 <632342527@qq.com>
2016-11-25 09:51:56 +0800

    Initial Commit

---
+++ 逐层刷新图片/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  逐层刷新图片
+//
+//  Created by 王奥东 on 16/11/25.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 逐层刷新图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  逐层刷新图片
+//
+//  Created by 王奥东 on 16/11/25.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
